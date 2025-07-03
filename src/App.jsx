e449791044c9f6e7dfe61aecbfc909159b8aaa12
Zeeshan Butt <zeeshanbutt11107@gmail.com>
2025-07-03 17:30:12 +0500

    Added Check File for practice

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import Data from "./Components/Data/Data";
 import Start from "./Components/Start/Start";
 import { Route, Routes } from "react-router-dom";
 import Contact from "./Components/Contact/Contact";
+import Check from "./Components/Contact/Check";
 
 //creat a layout file for layout related stuff e.g footer, header and main area, sidebar, create a Route.js file
 
@@ -19,6 +20,7 @@ function App() {
     <>
       <div className="bg-[#042036]">
         <div className="relative pt-20  border-[#053964]">
+          <Check/>
           <Navbar />
           <Routes>
             <Route path="/" element={<Start />} />
